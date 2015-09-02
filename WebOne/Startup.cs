e87626d9d25061338ec640ee1f6306dfc9bb5b2b
Scott W Jennen <scottwjennen@gmail.com>
2015-09-02 13:25:09 -0500

    Initialize

---
+++ WebOne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebOne.Startup))]
+namespace WebOne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
