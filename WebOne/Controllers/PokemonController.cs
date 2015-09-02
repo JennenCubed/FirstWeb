2ba2b35fde63d05778e59845f5517fd8c2cbab35
Scott W Jennen <scottwjennen@gmail.com>
2015-09-02 15:22:17 -0500

    SJennen -> PokemonIndex

---
+++ WebOne/Controllers/PokemonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+
+namespace WebOne.Controllers
+{
+    public class PokemonController : Controller
+    {
+        private PokemonDAL PDAL = new PokemonDAL();
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+            List<Pokemon> pokemon = PDAL.pokemon.ToList();
+            return View(pokemon);
+        }
+    }
+}
